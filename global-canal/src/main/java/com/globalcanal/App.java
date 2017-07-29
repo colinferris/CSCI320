2debec2bb3479a05b8c8a862736a166d8bc4499a
Colin Ferris <colin@colinferris.com>
2017-07-29 19:58:10 -0400

    Stubbed out command line interface.

--- global-canal/src/main/java/com/globalcanal/App.java
+++ global-canal/src/main/java/com/globalcanal/App.java
@@ -1,32 +1,19 @@
 package com.globalcanal;
-import java.util.Scanner;
 
 public class App {
 
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
+        Menu mainMenu = new Menu();
+        GlobalCanal gc = new GlobalCanal();
 
-        do {
-            System.out.println("Enter \"1\", \"2\", \"3\" or \"4\"");
-            choice = scanner.nextInt();
+        mainMenu.addMenuItem("1", "Product Search", new Runnable() {
+                    @Override
+                    public void run(){
+                        System.out.println("This is the second menu item");
+                        System.out.println("This menu item features a Runnable to define the function this menu item calls");
+                    }
+        });
 
-            switch (choice) {
-                case 1:
-                    System.out.println("1");
-                    break;
-                case 2:
-                    System.out.println("2");
-                    break;
-                case 3:
-                    System.out.println("3");
-                    break;
-                case 4:
-                    System.out.println("4");
-                    break;
-                default:
-            }
-        }
-        while (choice != 4);
+        mainMenu.start();
     }
 }
---
+++ global-canal/src/main/java/com/globalcanal/MenuItem.java
@@ -0,0 +1,30 @@
+package com.globalcanal;
+
+public class MenuItem {
+    private String key;
+    private String name;
+    private Runnable runnable;
+
+    public MenuItem(String key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public MenuItem(String key, String name, Runnable runnable) {
+        this.key = key;
+        this.name = name;
+        this.runnable = runnable;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Runnable getRunnable() {
+        return runnable;
+    }
+}
