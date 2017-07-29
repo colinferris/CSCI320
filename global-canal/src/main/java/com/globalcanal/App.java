5765ae8822a3df09e96c858bfc4fe09d6cf4c7c6
Colin Ferris <colin@colinferris.com>
2017-07-29 16:28:07 -0400

    Stubbed out command-line.

--- global-canal/src/main/java/com/globalcanal/App.java
+++ global-canal/src/main/java/com/globalcanal/App.java
@@ -1,13 +1,32 @@
 package com.globalcanal;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Enter \"1\", \"2\", \"3\" or \"4\"");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.println("3");
+                    break;
+                case 4:
+                    System.out.println("4");
+                    break;
+                default:
+            }
+        }
+        while (choice != 4);
     }
 }
