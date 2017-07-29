6b0d12bcd80056f681673120b7cd4686d97b2876
Colin Ferris <colin@colinferris.com>
2017-07-29 11:39:46 -0400

    Stubbed out main command line.

--- GlobalCanal/src/com/globalcanal/Main.java
+++ GlobalCanal/src/com/globalcanal/Main.java
@@ -1,8 +1,33 @@
 package com.globalcanal;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Enter \"1\", \"2\", \"3\" or \"4\"");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.println("3");
+                    break;
+                case 4:
+                    System.out.println("4");
+                    break;
+                default:
+            }
+        }
+        while (choice != 4);
     }
 }
