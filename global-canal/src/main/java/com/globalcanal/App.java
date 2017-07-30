1d9745beede1366b09cfd090b32321b0fe7a99c3
Colin Ferris <colin@colinferris.com>
2017-07-30 13:40:18 -0400

    Login

--- global-canal/src/main/java/com/globalcanal/App.java
+++ global-canal/src/main/java/com/globalcanal/App.java
@@ -1,18 +1,30 @@
 package com.globalcanal;
 import java.util.Scanner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class App {
+    private static Scanner scan;
+    private static GlobalCanal context;
+    private static Connection conn;
+    private static String location = "~/globalcanal/globalcanal";
 
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
+        scan = new Scanner(System.in);
+        context = new GlobalCanal();
+        login();
 
+        int choice;
         do {
             System.out.println("\n*** Menu Options ***\n");
             System.out.println("1 - Product Search");
 
             System.out.print("> ");
-            choice = scanner.nextInt();
+            choice = scan.nextInt();
 
             switch (choice) {
                 case 1:
@@ -33,5 +45,50 @@ public class App {
             }
         }
         while (choice != 4);
+    }
+
+    public static void createConnection(String location,
+                                 String user,
+                                 String password){
+        try {
+
+            //This needs to be on the front of your location
+            String url = "jdbc:h2:" + location;
+
+            //This tells it to use the h2 driver
+            Class.forName("org.h2.Driver");
+
+            //creates the connection
+            conn = DriverManager.getConnection(url,
+                    user,
+                    password);
+        } catch (SQLException e) {
+            //You should handle this better
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * When your database program exits
+     * you should close the connection
+     */
+    public void closeConnection(){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void login(){
+        System.out.println("Authentication is required.");
+        System.out.println("Enter Username:");
+        String username = scan.nextLine();
+        System.out.println("Enter Password:");
+        String password = scan.nextLine();
+
+        createConnection(location, username, password);
     }
 }
